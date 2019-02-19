cd8f1452c0e5fce6ae2f8b44439b246d6fd9dfd1
desshub <korirdesmond@MacBook-Pro-3.local>
2019-02-19 18:23:37 +0300

    Test toplist getters

--- tests/unit/store/toplist.spec.js
+++ tests/unit/store/toplist.spec.js
@@ -1,7 +1,7 @@
 import toplist from '../../../src/store/modules/toplist';
 import mockToplist from '../mock/toplist';
 
-const { mutations } = toplist;
+const { mutations, getters } = toplist;
 
 describe('Toplist mutations', () => {
   const { fetchingToplist, addToplistToStore, fetchingToplistFailed } = mutations;
@@ -30,5 +30,25 @@ describe('Toplist mutations', () => {
     const payload = 'Bad request.';
     fetchingToplistFailed(state, payload);
     expect(state.errorMessage).notToBeNull;
+  })
+});
+
+// Getters edge cases
+describe('Toplist getters', () => {
+  const { getToplist } = getters;
+  let state 
+
+  beforeEach(() => {
+    state = {
+      isFetching: false,
+      success: false,
+      errorMessage: null,
+      toplist: mockToplist,
+    };
+  });
+
+  it('getToplist', () => {
+    const toplistObj = getToplist(state);
+    expect(toplistObj.length).toBe(2);
   })
 })
